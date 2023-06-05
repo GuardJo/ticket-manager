207c0497f1779053220f69767ace255355f59436
kyeongho <dagh1218@naver.com>
2023-06-05 23:11:34 +0900

    #3 spring boot 기본 프로젝트 생성
    - spring boot 프로젝트 생성
    -- lombok
    -- mariadb, h2
    -- spring-data-jpa
    -- spring-batch
    관련 의존 추가

---
+++ ticket-manager-batch/src/main/java/com/guardjo/ticketmanager/batch/TicketManagerBatchApplication.java
@@ -0,0 +1,12 @@
+package com.guardjo.ticketmanager.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketManagerBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketManagerBatchApplication.class, args);
+    }
+}
