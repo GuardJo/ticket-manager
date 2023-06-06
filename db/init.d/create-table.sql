c43acea1ebb558d9e5fd1f30f4a5688442fa8f1c
kyeongho <dagh1218@naver.com>
2023-06-07 01:03:51 +0900

    #8 데이터베이스 구동 시 초기 데이터 구성
    - mariaDB 이미지 구동 시 초기데이터 삽입 설정 추가
    -- 초기데이터는 init.d/create-table.sql, insert-data.sql에 구성함
    -- 이 때 docker에서 init.d 내부 데이터를 읽을 때는 파일명의 사전순대로 읽기에 create 쿼리가 먼저오도록 구성해야함

---
+++ db/init.d/create-table.sql
@@ -0,0 +1,40 @@
+create table user(
+                     user_id bigint not null auto_increment primary key,
+                     name varchar(100) not null,
+                     status varchar(50) not null,
+                     phone_number varchar(50),
+                     email varchar(100),
+                     create_time timestamp not null default CURRENT_TIMESTAMP,
+                     modified_time timestamp not null default CURRENT_TIMESTAMP
+);
+
+create table package(
+                        package_id bigint not null auto_increment primary key,
+                        name varchar(100) not null,
+                        count int default null,
+                        expiration_period int default null,
+                        create_time timestamp not null default CURRENT_TIMESTAMP,
+                        modified_time timestamp not null default CURRENT_TIMESTAMP
+);
+
+create table ticket(
+                       ticket_id bigint not null auto_increment primary key,
+                       user_id bigint not null,
+                       package_id bigint not null,
+                       remaining_count int default 0,
+                       started_time timestamp not null,
+                       expired_time timestamp not null,
+                       created_time timestamp not null default CURRENT_TIMESTAMP,
+                       modified_time timestamp not null default CURRENT_TIMESTAMP
+);
+
+create table reservation(
+                            reservation_id bigint not null auto_increment primary key,
+                            user_id bigint not null,
+                            ticket_id bigint not null,
+                            used_count int default 0,
+                            started_time timestamp not null,
+                            finished_time timestamp not null,
+                            created_time timestamp not null default CURRENT_TIMESTAMP,
+                            modified_time timestamp not null default CURRENT_TIMESTAMP
+);
