14cb44a8361e0854be7d4c1ce01896c53e888afa
kyeongho <dagh1218@naver.com>
2023-06-10 20:30:49 +0900

    #10 설계한 데이터들에 대한 연관관계 구성
    - 연관관계 구성 및 Entity 구조 구성
    -- 구성된 구조 readme에 추가
    - 일부 컬럼명 오타 수정 (create_time -> created_time)
    - 회원 테이블명 변경 (user -> member)
    -- 예약어 이슈로 자꾸 걸리기에 변경함

--- db/init.d/create-table.sql
+++ db/init.d/create-table.sql
@@ -1,11 +1,11 @@
-create table user
+create table member
 (
-    user_id       bigint       not null auto_increment primary key,
+    member_id       bigint       not null auto_increment primary key,
     name          varchar(100) not null,
     status        varchar(50)  not null,
     phone_number  varchar(50),
     email         varchar(100),
-    create_time   timestamp    not null default CURRENT_TIMESTAMP,
+    created_time  timestamp    not null default CURRENT_TIMESTAMP,
     modified_time timestamp    not null default CURRENT_TIMESTAMP
 );
 
@@ -15,30 +15,38 @@ create table program
     name              varchar(100) not null,
     count             int                   default null,
     expiration_period int                   default null,
-    create_time       timestamp    not null default CURRENT_TIMESTAMP,
+    created_time      timestamp    not null default CURRENT_TIMESTAMP,
     modified_time     timestamp    not null default CURRENT_TIMESTAMP
 );
 
 create table ticket
 (
     ticket_id       bigint    not null auto_increment primary key,
-    user_id         bigint    not null,
+    member_id         bigint    not null,
     program_id      bigint    not null,
     remaining_count int                default 0,
     started_time    timestamp not null,
     expired_time    timestamp not null,
     created_time    timestamp not null default CURRENT_TIMESTAMP,
-    modified_time   timestamp not null default CURRENT_TIMESTAMP
+    modified_time   timestamp not null default CURRENT_TIMESTAMP,
+    constraint FKd5t3axsm3pj3mt41qhpkmw9yj
+        foreign key (member_id) references member (member_id),
+    constraint FKliq3sqxtuknpgr7rsmufx5p12
+        foreign key (program_id) references program (program_id)
 );
 
 create table reservation
 (
     reservation_id bigint    not null auto_increment primary key,
-    user_id        bigint    not null,
+    member_id        bigint    not null,
     ticket_id      bigint    not null,
     used_count     int                default 0,
     started_time   timestamp not null,
     finished_time  timestamp not null,
     created_time   timestamp not null default CURRENT_TIMESTAMP,
-    modified_time  timestamp not null default CURRENT_TIMESTAMP
+    modified_time  timestamp not null default CURRENT_TIMESTAMP,
+    constraint FK68999qe28ym9eqqlowybh9nvn
+        foreign key (member_id) references member (member_id),
+    constraint FKkn72g30lgqj1a31doyr5qb6ln
+        foreign key (ticket_id) references ticket (ticket_id)
 );
--- db/init.d/insert-data.sql
+++ db/init.d/insert-data.sql
@@ -9,11 +9,11 @@ values (4, '수영 3개월권', null, 90);
 insert into program (program_id, name, count, expiration_period)
 values (5, '헬스장 1개월권', null, 30);
 
-insert into user (user_id, name, status, phone_number, email)
+insert into member (member_id, name, status, phone_number, email)
 values (1, 'tester', 'ACTIVE', '010-1010-1011', 'test@mail.com');
-insert into user (user_id, name, status, phone_number, email)
+insert into member (member_id, name, status, phone_number, email)
 values (2, 'tester2', 'INACTIVE', '010-1010-1012', 'test2@mail.com');
-insert into user (user_id, name, status, phone_number, email)
+insert into member (member_id, name, status, phone_number, email)
 values (3, 'tester3', 'ACTIVE', '010-1010-1013', 'test3@mail.com');
-insert into user (user_id, name, status, phone_number, email)
+insert into member (member_id, name, status, phone_number, email)
 values (4, 'tester4', 'ACTIVE', '010-1010-1014', 'test4@mail.com');
