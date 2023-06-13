3957dbc69905c8d5231adbda7b7edd1da3776d73
kyeongho <dagh1218@naver.com>
2023-06-13 22:58:09 +0900

    #13 이용권 기한 만료 Job 구현
    - 이용권 기간 만료 시 상태를 EXPIRED 로 변경하는 Job 구성
    -- 데이터 정합성을 보장하기 위해 JpaCursorReader로 읽어들임

---
+++ ticket-manager-batch/src/main/java/com/guardjo/ticketmanager/batch/job/ExpireTicketJobConfig.java
@@ -0,0 +1,80 @@
+package com.guardjo.ticketmanager.batch.job;
+
+import com.guardjo.ticketmanager.batch.domain.Ticket;
+import com.guardjo.ticketmanager.batch.domain.TicketStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.JpaCursorItemReader;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class ExpireTicketJobConfig {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+
+    private final static int CHUNK_SIZE = 5;
+
+    @Bean
+    public Job expireTicketJob() {
+        return jobBuilderFactory.get("expireTicketJob")
+                .start(expireTicketStep())
+                .build();
+    }
+
+    @Bean
+    public Step expireTicketStep() {
+        return stepBuilderFactory.get("expireTicketStep")
+                .<Ticket, Ticket>chunk(CHUNK_SIZE)
+                .reader(ticketItemReader())
+                .processor(ticketItemProcessor())
+                .writer(ticketItemWriter())
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public JpaCursorItemReader<Ticket> ticketItemReader() {
+        return new JpaCursorItemReaderBuilder<Ticket>()
+                .name("ticketItemReader")
+                .entityManagerFactory(entityManagerFactory)
+                .queryString("select t from Ticket t where (t.status = :ready or t.status = :progress) and t.expiredTime <= :expiredTime")
+                .parameterValues(
+                        Map.of(
+                                "ready", TicketStatus.READY,
+                                "progress", TicketStatus.PROGRESS,
+                                "expiredTime", LocalDateTime.now()))
+                .build();
+    }
+
+    @Bean
+    public ItemProcessor<Ticket, Ticket> ticketItemProcessor() {
+        return (ticket -> {
+            ticket.setStatus(TicketStatus.EXPIRED);
+            ticket.setExpiredTime(LocalDateTime.now());
+            return ticket;
+        });
+    }
+
+    @Bean
+    public JpaItemWriter<Ticket> ticketItemWriter() {
+        return new JpaItemWriterBuilder<Ticket>()
+                .entityManagerFactory(entityManagerFactory)
+                .build();
+
+    }
+}
