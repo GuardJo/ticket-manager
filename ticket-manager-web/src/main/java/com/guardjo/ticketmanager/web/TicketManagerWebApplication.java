6ae5e22d6a53811f5a4f6e3a6ccdf3ce59bfaa18
kyeongho <dagh1218@naver.com>
2023-08-14 23:30:26 +0900

    #31 thymeleaf decoupling logic 옵션 활성화
    - thymleaf 코드를 분리 작업할 수 있도록 설정 활성화

--- ticket-manager-web/src/main/java/com/guardjo/ticketmanager/web/TicketManagerWebApplication.java
+++ ticket-manager-web/src/main/java/com/guardjo/ticketmanager/web/TicketManagerWebApplication.java
@@ -2,7 +2,9 @@ package com.guardjo.ticketmanager.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class TicketManagerWebApplication {
 
