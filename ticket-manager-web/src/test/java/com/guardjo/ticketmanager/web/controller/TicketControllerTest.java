f7b9f479482c9fbe59bccb433754b01057915217
kyeongho <dagh1218@naver.com>
2023-08-28 23:48:53 +0900

    #31 이용권 예약 현황 조회 페이지 view controller 구혐
    - 앞서 구현된 이용권 예약 목록 조회 서비스 객체 호출을 통한 관련 view data 조회 페이지 반환 구현
    -- 관련 controller test 구성

--- ticket-manager-web/src/test/java/com/guardjo/ticketmanager/web/controller/TicketControllerTest.java
+++ ticket-manager-web/src/test/java/com/guardjo/ticketmanager/web/controller/TicketControllerTest.java
@@ -1,25 +1,43 @@
 package com.guardjo.ticketmanager.web.controller;
 
+import com.guardjo.ticketmanager.web.data.TicketViewData;
+import com.guardjo.ticketmanager.web.service.ReservationService;
+import com.guardjo.ticketmanager.web.util.TestDataGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = TicketController.class)
 class TicketControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private ReservationService reservationService;
+
     @DisplayName("예약 현황 조회 페이지 반환 테스트")
     @Test
     void testListPage() throws Exception {
+        List<TicketViewData> ticketViewDataList = List.of(TestDataGenerator.ticketViewData(1L));
+
+        given(reservationService.findReservationTickets()).willReturn(ticketViewDataList);
+
         mockMvc.perform(get("/tickets"))
                 .andExpect(status().isOk())
+                .andExpect(model().attributeExists("tickets"))
                 .andExpect(view().name("/tickets/list"));
+
+        then(reservationService).should().findReservationTickets();
     }
 }
