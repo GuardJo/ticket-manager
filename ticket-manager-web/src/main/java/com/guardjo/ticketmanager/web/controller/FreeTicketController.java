e44f8c7c688de3c4d99059c8248ee662c0098d48
kyeongho <dagh1218@naver.com>
2023-09-03 22:49:17 +0900

    #41 예약 뷰 페이지 내 Thymeleaf 템플릿 구성
    - 서버로부터 전달받는 관련 DTO들에 대한 Thymeleaf 매핑
    -- 매핑 결과 확인을 위해 임시 DTO 객체를 반환하도록 구성 cotnroller 구성

--- ticket-manager-web/src/main/java/com/guardjo/ticketmanager/web/controller/FreeTicketController.java
+++ ticket-manager-web/src/main/java/com/guardjo/ticketmanager/web/controller/FreeTicketController.java
@@ -1,16 +1,42 @@
 package com.guardjo.ticketmanager.web.controller;
 
+import com.guardjo.ticketmanager.web.data.FreeTicketViewData;
+import com.guardjo.ticketmanager.web.data.TicketSimpleData;
+import com.guardjo.ticketmanager.web.data.UserGroupSimpleData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RequestMapping("/reservation")
 @Controller
 @Slf4j
 public class FreeTicketController {
     @GetMapping
-    public String getFreeTickets() {
+    public String getFreeTickets(ModelMap modelMap) {
+        List<FreeTicketViewData> freeTicketViewDataList = List.of(
+                FreeTicketViewData.create(1, "test", "test", "test", LocalDateTime.now()),
+                FreeTicketViewData.create(2, "test2", "Test2", "test", LocalDateTime.now())
+        );
+
+        List<TicketSimpleData> ticketSimpleDataList = List.of(
+                TicketSimpleData.create(1, "testTicket1"),
+                TicketSimpleData.create(2, "testTicket2")
+        );
+
+        List<UserGroupSimpleData> userGroupSimpleDataList = List.of(
+                UserGroupSimpleData.create(1, "group1"),
+                UserGroupSimpleData.create(2, "group2")
+        );
+
+        modelMap.addAttribute("freeTickets", freeTicketViewDataList);
+        modelMap.addAttribute("tickets", ticketSimpleDataList);
+        modelMap.addAttribute("userGroups", userGroupSimpleDataList);
+
         return "/tickets/reservation";
     }
 }
