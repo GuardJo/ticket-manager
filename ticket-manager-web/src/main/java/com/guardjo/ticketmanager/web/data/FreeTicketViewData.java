1de32dc70fc96e052b76c97ce4479bcd2a164adb
kyeongho <dagh1218@naver.com>
2023-09-03 22:47:40 +0900

    #41 예약 뷰 페이지용 DTO 객체 구성
    - 신규 이용권 발급 요청 시 기존 이용권 관련 DTO, 이용자 그룹 관련 DTO 구성
    - 이용권 발급 현황 목록에 주입할 무료 이용권 관련 DTO 구성

---
+++ ticket-manager-web/src/main/java/com/guardjo/ticketmanager/web/data/FreeTicketViewData.java
@@ -0,0 +1,28 @@
+package com.guardjo.ticketmanager.web.data;
+
+import io.github.guardjo.ticketmanager.common.domain.FreeTicket;
+
+import java.time.LocalDateTime;
+
+public record FreeTicketViewData(
+        long freeTicketId,
+        String programName,
+        String userGroupName,
+        String status,
+        LocalDateTime createdTime
+) {
+    public static FreeTicketViewData create(long freeTicketId, String programName, String userGroupName,
+                                            String status, LocalDateTime createdTime) {
+        return new FreeTicketViewData(freeTicketId, programName, userGroupName, status, createdTime);
+    }
+
+    public static FreeTicketViewData from(FreeTicket freeTicket) {
+        return FreeTicketViewData.create(
+                freeTicket.getId(),
+                freeTicket.getTicket().getProgram().getName(),
+                freeTicket.getMemberGroup().getGroupName(),
+                freeTicket.getStatus().name(),
+                freeTicket.getTicket().getStartedTime()
+        );
+    }
+}
